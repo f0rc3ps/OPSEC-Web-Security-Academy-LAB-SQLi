bd8c317c485108ad1d302756fe2e9cd98de74765
r@bb1th01e <bugb0untypropentest@gmail.com>
2024-10-10 17:03:13 +0300

    Update dbconnection.php

--- SQLi/www/php-scrm/dbconnection.php
+++ SQLi/www/php-scrm/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("db", "user", "test", "scrm_db");
+$con=mysqli_connect("localhost", "root", "test", "scrm_db");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
