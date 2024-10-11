6985e1e1ff88930b1556275d14f469e6fe25ee8f
nu11secur1ty <bugb0untypropentest@gmail.com>
2024-10-11 14:03:35 +0300

    Up
    Up

--- SQLi/dump/cms_db.sql
+++ SQLi/dump/cms_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 16, 2022 at 09:19 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Host: db
+-- Generation Time: Oct 11, 2024 at 11:01 AM
+-- Server version: 8.0.39
+-- PHP Version: 8.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,16 +28,16 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `booking_list` (
-  `id` int(30) NOT NULL,
+  `id` int NOT NULL,
   `ref_code` varchar(100) NOT NULL,
-  `client_id` int(30) NOT NULL,
-  `cab_id` int(30) NOT NULL,
+  `client_id` int NOT NULL,
+  `cab_id` int NOT NULL,
   `pickup_zone` text NOT NULL,
   `drop_zone` text NOT NULL,
-  `status` tinyint(2) NOT NULL DEFAULT 0 COMMENT '0 = Pending,\r\n1 = Driver has Confirmed,\r\n2 = Pickup,\r\n3 = drop-off,\r\n4 = cancelled',
-  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status` tinyint NOT NULL DEFAULT '0' COMMENT '0 = Pending,\r\n1 = Driver has Confirmed,\r\n2 = Pickup,\r\n3 = drop-off,\r\n4 = cancelled',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `booking_list`
@@ -54,9 +54,9 @@ INSERT INTO `booking_list` (`id`, `ref_code`, `client_id`, `cab_id`, `pickup_zon
 --
 
 CREATE TABLE `cab_list` (
-  `id` int(30) NOT NULL,
+  `id` int NOT NULL,
   `reg_code` varchar(100) NOT NULL,
-  `category_id` int(30) NOT NULL,
+  `category_id` int NOT NULL,
   `cab_reg_no` varchar(200) NOT NULL,
   `body_no` varchar(100) NOT NULL,
   `cab_model` text NOT NULL,
@@ -64,12 +64,12 @@ CREATE TABLE `cab_list` (
   `driver_contact` text NOT NULL,
   `driver_address` text NOT NULL,
   `password` text NOT NULL,
-  `image_path` text DEFAULT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 1,
-  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `image_path` text,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `cab_list`
@@ -86,14 +86,14 @@ INSERT INTO `cab_list` (`id`, `reg_code`, `category_id`, `cab_reg_no`, `body_no`
 --
 
 CREATE TABLE `category_list` (
-  `id` int(30) NOT NULL,
+  `id` int NOT NULL,
   `name` text NOT NULL,
   `description` text NOT NULL,
-  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
-  `status` tinyint(1) NOT NULL DEFAULT 1,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `category_list`
@@ -110,21 +110,21 @@ INSERT INTO `category_list` (`id`, `name`, `description`, `delete_flag`, `status
 --
 
 CREATE TABLE `client_list` (
-  `id` int(30) NOT NULL,
+  `id` int NOT NULL,
   `firstname` text NOT NULL,
-  `middlename` text DEFAULT NULL,
+  `middlename` text,
   `lastname` text NOT NULL,
   `gender` text NOT NULL,
   `contact` text NOT NULL,
   `address` text NOT NULL,
   `email` text NOT NULL,
   `password` text NOT NULL,
-  `image_path` text DEFAULT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 1,
-  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_added` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `image_path` text,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `delete_flag` tinyint(1) NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_added` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `client_list`
@@ -140,10 +140,10 @@ INSERT INTO `client_list` (`id`, `firstname`, `middlename`, `lastname`, `gender`
 --
 
 CREATE TABLE `system_info` (
-  `id` int(30) NOT NULL,
+  `id` int NOT NULL,
   `meta_field` text NOT NULL,
   `meta_value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `system_info`
@@ -163,17 +163,17 @@ INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(50) NOT NULL,
+  `id` int NOT NULL,
   `firstname` varchar(250) NOT NULL,
   `lastname` varchar(250) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL,
-  `avatar` text DEFAULT NULL,
+  `avatar` text,
   `last_login` datetime DEFAULT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT 0,
-  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `type` tinyint(1) NOT NULL DEFAULT '0',
+  `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
@@ -207,82 +207,6 @@ ALTER TABLE `cab_list`
 --
 ALTER TABLE `category_list`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `client_list`
---
-ALTER TABLE `client_list`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`) USING HASH;
-
---
--- Indexes for table `system_info`
---
-ALTER TABLE `system_info`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `booking_list`
---
-ALTER TABLE `booking_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `cab_list`
---
-ALTER TABLE `cab_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `category_list`
---
-ALTER TABLE `category_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `client_list`
---
-ALTER TABLE `client_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `system_info`
---
-ALTER TABLE `system_info`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `booking_list`
---
-ALTER TABLE `booking_list`
-  ADD CONSTRAINT `booking_list_ibfk_1` FOREIGN KEY (`cab_id`) REFERENCES `cab_list` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `booking_list_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `client_list` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `cab_list`
---
-ALTER TABLE `cab_list`
-  ADD CONSTRAINT `cab_list_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category_list` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
