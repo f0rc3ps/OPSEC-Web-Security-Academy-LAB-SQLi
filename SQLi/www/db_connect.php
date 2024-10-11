e202177776f8d32b40f610cf1689d35192443acc
nu11secur1ty <bugb0untypropentest@gmail.com>
2024-10-11 11:34:49 +0300

    Up
    Up

--- SQLi/www/db_connect.php
+++ SQLi/www/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('db','root','test','cms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','test','cms_db')or die("Could not connect to mysql".mysqli_error($con));
